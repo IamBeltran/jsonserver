049df4a859d6ef3fd287595f0a1f902de91faf33
Victor Giovanni Beltrán Rodríguez <beltran-victor@outlook.com>
2019-07-20 02:26:06 -0500

    style: add documentation of functions

--- src/services/handletoken/index.js
+++ src/services/handletoken/index.js
@@ -98,6 +98,17 @@ function hasProperty(object, property) {
  */
 
 /**
+ * @typedef       {Object} PayloadToken
+ * @property      {string} sub - Token Subject
+ * @property      {number} iat - Token Issued at
+ * @property      {number} exp - Token Expiration time
+ * @property      {number} nbf - Token Not before
+ * @property      {string} aud - Token Audience
+ * @property      {string} iss - Token Issued
+ * @property      {string} jti - Token ID
+ */
+
+/**
  * @name          createToken
  * @description   Function to create user tokens
  * @param         {User} user - User information
@@ -112,9 +123,9 @@ function createToken(user, reqIssuer) {
 
 /**
  * @name          getPayloadToken
- * @description   Function to create user tokens
+ * @description   Function to return a Payload of a Token
  * @param         {string} token - A token
- * @returns       {Object} Return a Token
+ * @returns       {PayloadToken} Payload of a Token, or a error is not valid Token
  */
 function getPayloadToken(token) {
   return jwt.verify(token, keyApi, options.verify, (err, decode) =>
@@ -122,15 +133,27 @@ function getPayloadToken(token) {
   );
 }
 
-const isValidToken = token => {
+/**
+ * @name          isValidToken
+ * @description   Function to ckecked is valid token
+ * @param         {string} token - A token
+ * @returns       {Boolean} Return true if valid token, or false is not
+ */
+function isValidToken(token) {
   const payload = jwt.verify(token, keyApi, options.verify, (err, decode) =>
     decode !== undefined ? decode : err,
   );
   const hasErrorToken = hasProperty(payload, 'name') && errorsToken.includes(payload.name);
   return hasErrorToken ? false : isValidIdUser(payload.sub);
-};
+}
 
-const decodeToken = token => {
+/**
+ * @name          decodeToken
+ * @description   Function to return a HTTP response
+ * @param         {string} token - A token
+ * @returns       {Promise} Return a valid HTTP response if valid token, or invalid HTTP response is not
+ */
+function decodeToken(token) {
   const decoded = new Promise((resolve, reject) => {
     const payload = jwt.verify(token, keyApi, options.verify, (err, decode) =>
       decode !== undefined ? decode : err,
@@ -161,7 +184,7 @@ const decodeToken = token => {
         });
   });
   return decoded;
-};
+}
 
 //  ──[ EXPORT MODULE ]──────────────────────────────────────────────────────────────────
 const handleToken = (module.exports = exports = {}); // eslint-disable-line no-multi-assign
