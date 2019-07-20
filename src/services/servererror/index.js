4d1418a7779d85670abd2991cca99b3a224a30f7
Victor Giovanni Beltrán Rodríguez <beltran-victor@outlook.com>
2019-07-20 00:51:53 -0500

    feat: add ServerError

---
+++ src/services/servererror/index.js
@@ -0,0 +1,63 @@
+//  ┌───────────────────────────────────────────────────────────────────────────────────┐
+//  │ REQUIRE THIRDPARTY DEPENDENCIES MODULES.                                          │
+//  └───────────────────────────────────────────────────────────────────────────────────┘
+
+//  ┌───────────────────────────────────────────────────────────────────────────────────┐
+//  │ REQUIRE NODEJS DEPENDENCIES MODULE.                                               │
+//  └───────────────────────────────────────────────────────────────────────────────────┘
+
+//  ┌───────────────────────────────────────────────────────────────────────────────────┐
+//  │ REQUIRE MY DEPENDENCIES MODULES.                                                  │
+//  └───────────────────────────────────────────────────────────────────────────────────┘
+
+//  ┌───────────────────────────────────────────────────────────────────────────────────┐
+//  │ DESTRUCTURING DEPENDENCIES.                                                       │
+//  └───────────────────────────────────────────────────────────────────────────────────┘
+
+//  ┌───────────────────────────────────────────────────────────────────────────────────┐
+//  │ DECLARATION OF CONSTANTS-VARIABLES.                                               │
+//  └───────────────────────────────────────────────────────────────────────────────────┘
+const ServerErrors = {};
+
+//  ┌───────────────────────────────────────────────────────────────────────────────────┐
+//  │ DECLARATION OF AUXILIARY FUNCTIONS.                                               │
+//  └───────────────────────────────────────────────────────────────────────────────────┘
+
+//  ┌───────────────────────────────────────────────────────────────────────────────────┐
+//  │ SET MAIN MODULE - SERVER ERROR.                                                   │
+//  └───────────────────────────────────────────────────────────────────────────────────┘
+/**
+ * @author        Victor Giovanni Beltrán Rodríguez
+ * @version       2.0.0
+ * @description   Create a new ServerError
+ * @class         ServerError
+ * @extends       {Error}
+ */
+class ServerError extends Error {
+  /**
+   * @param   {String} [error='Default error message'] - Name of error
+   * @param   {Object} addInfo - Additional error information
+   */
+  constructor(error = 'Default error message', addInfo) {
+    super();
+    const ERRORS = ServerErrors;
+    const message = ERRORS[`${error}`] ? ERRORS[`${error}`] : error;
+    this.name = 'ServerError';
+    this.message = message;
+    const $this = this;
+    if (addInfo && Object.entries(addInfo).length !== 0 && addInfo.constructor) {
+      Object.getOwnPropertyNames(addInfo).forEach(value => {
+        if (value === 'name') {
+          $this.originalNameError = addInfo[value];
+        } else if (value === 'stack') {
+          $this.originalStackError = addInfo[value];
+        } else {
+          $this[value] = addInfo[value];
+        }
+      });
+    }
+  }
+}
+
+//  ──[ EXPORT MODULE ]──────────────────────────────────────────────────────────────────
+module.exports = ServerError;
