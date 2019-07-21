48c64d6b198d6103b54afdb649973dd013259e3f
Victor Giovanni Beltrán Rodríguez <beltran-victor@outlook.com>
2019-07-21 01:25:32 -0500

    refactor: change serverErrors

---
+++ src/services/servererror/servererrors.js
@@ -0,0 +1,10 @@
+//  ┌───────────────────────────────────────────────────────────────────────────────────┐
+//  │ DECLARATION OF CONSTANTS-VARIABLES.                                               │
+//  └───────────────────────────────────────────────────────────────────────────────────┘
+const ServerErrors = {
+  ERROR_REQUEST_NULL: 'ARGUMENTS NULL',
+  ERROR_ARGUMENTS_WRONG: 'ARGUMENTS WRONG',
+};
+
+//  ──[ EXPORT MODULE ]──────────────────────────────────────────────────────────────────
+module.exports = ServerErrors;
