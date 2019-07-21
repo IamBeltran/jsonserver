bab18ef3b32324a86fc0c9ff3a5360a55be5354b
Victor Giovanni Beltrán Rodríguez <beltran-victor@outlook.com>
2019-07-20 21:22:32 -0500

    feat: add session middleware

---
+++ src/middlewares/session/index.js
@@ -0,0 +1,64 @@
+//  ┌───────────────────────────────────────────────────────────────────────────────────┐
+//  │ REQUIRE THIRDPARTY DEPENDENCIES MODULES.                                          │
+//  └───────────────────────────────────────────────────────────────────────────────────┘
+const session = require('cookie-session');
+const Keygrip = require('keygrip');
+const moment = require('moment');
+
+//  ┌───────────────────────────────────────────────────────────────────────────────────┐
+//  │ REQUIRE NODEJS DEPENDENCIES MODULE.                                               │
+//  └───────────────────────────────────────────────────────────────────────────────────┘
+const path = require('path');
+const fs = require('fs');
+
+//  ──[ UTILS.  ]────────────────────────────────────────────────────────────────────────
+const appDirectory = fs.realpathSync(process.cwd());
+const resolveApp = relativePath => path.resolve(appDirectory, relativePath);
+
+//  ┌───────────────────────────────────────────────────────────────────────────────────┐
+//  │ REQUIRE MY DEPENDENCIES MODULES.                                                  │
+//  └───────────────────────────────────────────────────────────────────────────────────┘
+//  ──[ PATHS MODULES.  ]────────────────────────────────────────────────────────────────
+const configurations = resolveApp('configuration');
+
+//  ──[ LOGGERS.  ]──────────────────────────────────────────────────────────────────────
+const configuration = require(configurations);
+
+//  ┌───────────────────────────────────────────────────────────────────────────────────┐
+//  │ DESTRUCTURING DEPENDENCIES.                                                       │
+//  └───────────────────────────────────────────────────────────────────────────────────┘
+const {
+  SERVER: { SERVER_KEY_API: keyApi, SERVER_BASE_URL: baseUrl },
+} = configuration;
+
+//  ┌───────────────────────────────────────────────────────────────────────────────────┐
+//  │ DECLARATION OF CONSTANTS-VARIABLES.                                               │
+//  └───────────────────────────────────────────────────────────────────────────────────┘
+const expiry = moment().add(1, 'days'); // 7 days
+const options = {
+  name: 'session_app',
+  secret: keyApi,
+  keys: new Keygrip(['key1', 'key2'], 'SHA384', 'base64'),
+  resave: true,
+  saveUninitialized: true,
+  cookie: {
+    expires: expiry,
+    domain: baseUrl,
+    secure: true,
+    httpOnly: true,
+    sameSite: false,
+    signed: true,
+    overwrite: true,
+  },
+};
+
+//  ┌───────────────────────────────────────────────────────────────────────────────────┐
+//  │ DECLARATION OF AUXILIARY FUNCTIONS.                                               │
+//  └───────────────────────────────────────────────────────────────────────────────────┘
+
+//  ┌───────────────────────────────────────────────────────────────────────────────────┐
+//  │ SET MAIN MODULE - [NAME-MODULE].                                                  │
+//  └───────────────────────────────────────────────────────────────────────────────────┘
+
+//  ──[ EXPORT MODULE ]──────────────────────────────────────────────────────────────────
+module.exports = session(options);
